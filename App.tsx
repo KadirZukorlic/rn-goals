b35e2d3dfac5dbc89aeb1e9cff7019bacabcd349
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-06 17:51:38 +0100

    Initial

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import GoalInput from './components/GoalInput'
+import GoalItem from './components/GoalItem'
+
+export interface GoalList {
+	text: string
+	id: string
+}
+
+export default function App() {
+	const [goalList, setGoalList] = useState<GoalList[]>([])
+
+	const addGoalHandler = (enteredGoalText: string) => {
+		setGoalList((prevState) => [
+			...prevState,
+			{ text: enteredGoalText, id: Math.random().toString() }
+		])
+	}
+
+	return (
+		<View style={styles.appContainer}>
+			<GoalInput onAddGoal={addGoalHandler} />
+			<View style={styles.goalsContainer}>
+				<FlatList
+					data={goalList}
+					renderItem={(itemData) => <GoalItem itemData={itemData} />}
+					keyExtractor={(item) => item.id}
+					alwaysBounceVertical={false}
+				/>
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	appContainer: {
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 16
+	},
+
+	goalsContainer: {
+		flex: 5
+	}
+})
